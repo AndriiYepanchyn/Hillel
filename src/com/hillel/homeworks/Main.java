0add780f2f542e97ac0463f4fb0ed11bdb110f65
Yepanchyn <ayepanchyn@luxoft.com>
2024-06-28 16:56:21 +0300

    Update Main for lesson 9

--- src/com/hillel/homeworks/Main.java
+++ src/com/hillel/homeworks/Main.java
@@ -8,6 +8,7 @@ import com.hillel.homeworks.lesson_05.Lesson_05_Basic_algorithm_tasks;
 import com.hillel.homeworks.lesson_06.Lesson_06_Tasks_with_HashMap;
 import com.hillel.homeworks.lesson_07.Lesson_07_Tasks_with_Arrays_DataTime_Sorting_Collections;
 import com.hillel.homeworks.lesson_08.Lesson_08_Trees;
+import com.hillel.homeworks.lesson_09.Lesson_09_TreeMap_Jenerics;
 
 /*
  * @Author: Andrii Yepanchyn
@@ -27,7 +28,8 @@ public class Main {
 //        Lesson_05_Basic_algorithm_tasks.run();
 //        Lesson_06_Tasks_with_HashMap.run();
 //        Lesson_07_Tasks_with_Arrays_DataTime_Sorting_Collections.run();
-        Lesson_08_Trees.run();
+//        Lesson_08_Trees.run();
+        Lesson_09_TreeMap_Jenerics.run();
 
     }
 }
